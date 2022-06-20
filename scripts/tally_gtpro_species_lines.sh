a0a344611e06f008a0f7f07fa63e1f8d57326640
Byron Smith <me@byronjsmith.com>
2022-06-19 22:03:49 -0700

    Fix incorrectness of tallying species lines.

--- scripts/tally_gtpro_species_lines.sh
+++ scripts/tally_gtpro_species_lines.sh
@@ -4,7 +4,9 @@ library=$1
 cat \
     <(bzcat "$2" | sed '1,1d') \
     <(bzcat "$3" | sed '1,1d') \
-    | cut -f1 \
+    | cut -f1,2 \
     | sort \
+    | uniq \
+    | cut -f1 \
     | uniq -c \
     | awk -v OFS='\t' -v library=$library 'NR>1{print library,$2,$1}'
