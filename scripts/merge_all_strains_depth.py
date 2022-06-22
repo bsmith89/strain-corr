99c7b65748a4f7b203d06f0aba27415b8195c84c
Byron Smith <me@byronjsmith.com>
2022-06-22 12:58:48 -0700

    Fix deprecation warning.

--- scripts/merge_all_strains_depth.py
+++ scripts/merge_all_strains_depth.py
@@ -13,9 +13,14 @@ if __name__ == "__main__":
     }
 
     # Load species level coverage.
-    motu_cv = pd.read_table(
-        species_path, index_col=["sample", "species_id"],
-    ).squeeze().unstack("species_id", fill_value=0)
+    motu_cv = (
+        pd.read_table(
+            species_path,
+            index_col=["sample", "species_id"],
+        )
+        .squeeze()
+        .unstack("species_id", fill_value=0)
+    )
     motu_cv.columns = motu_cv.columns.astype(str)
     assert motu_cv.index.is_unique
     assert motu_cv.columns.is_unique
@@ -29,9 +34,10 @@ if __name__ == "__main__":
         path = species_strains_path_map[motu]
         frac = (
             pd.read_table(
-                path, index_col=["sample", "strain"], squeeze=True,
+                path,
+                index_col=["sample", "strain"],
             )
-            .unstack("strain", fill_value=0)
+            .squeeze.unstack("strain", fill_value=0)
             .reindex(sample_list, fill_value=0)
         )
         if motu not in motu_cv.columns:
@@ -51,7 +57,7 @@ if __name__ == "__main__":
         pd.concat(sotu_cv.values())
         .unstack("sotu", fill_value=0)
         .join(motu_cv_missing)
-        .rename_axis(columns='strain')
+        .rename_axis(columns="strain")
     )
     # FIXME: Some of the '-other' strains are < 0 because of rounding error.
     sotu_cv[(sotu_cv < 0)] = 0
@@ -60,4 +66,6 @@ if __name__ == "__main__":
     assert sotu_cv.columns.is_unique
     assert (sotu_cv >= 0).all().all()
 
-    sotu_cv.stack().rename('depth')[lambda x: x > 0].to_csv(outpath, sep="\t", header=True)
+    sotu_cv.stack().rename("depth")[lambda x: x > 0].to_csv(
+        outpath, sep="\t", header=True
+    )
