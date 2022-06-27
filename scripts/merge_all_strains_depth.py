54a1a41a01fcec5d793e0212cc1ef75b873daf94
Byron Smith <me@byronjsmith.com>
2022-06-27 16:42:19 -0700

    Re-write strain rabund merging script.

--- scripts/merge_all_strains_depth.py
+++ scripts/merge_all_strains_depth.py
@@ -3,69 +3,71 @@
 import pandas as pd
 import sys
 from tqdm import tqdm
+import numpy as np
 
+
+DUMMY_STRAIN_LABEL = '-1'
+MINIMUM_FRAC = 1e-3
 
 if __name__ == "__main__":
     species_path = sys.argv[1]
     outpath = sys.argv[2]
-    species_strains_path_map = {
+    species_path_map = {
         k: v for k, v in [arg.strip().split("=") for arg in sys.argv[3:]]
     }
 
     # Load species level coverage.
-    motu_cv = (
+    species_depth = (
         pd.read_table(
             species_path,
+            dtype=dict(species_id=str, sample=str, depth=float),
             index_col=["sample", "species_id"],
         )
         .squeeze()
         .unstack("species_id", fill_value=0)
     )
-    motu_cv.columns = motu_cv.columns.astype(str)
-    assert motu_cv.index.is_unique
-    assert motu_cv.columns.is_unique
+    species_depth.columns = species_depth.columns.astype(str)
+    assert species_depth.index.is_unique
+    assert species_depth.columns.is_unique
 
-    sample_list = motu_cv.index
+    sample_list = species_depth.index
+    species_list = species_depth.columns
 
     # Compile intra-species relative abundance multiplied by the species
     # total coverage.
-    sotu_cv = {}
-    for motu in tqdm(species_strains_path_map):
-        path = species_strains_path_map[motu]
-        frac = (
-            pd.read_table(
-                path,
-                index_col=["sample", "strain"],
+    strain_depth = {}
+    for species in tqdm(species_list):
+        if species in species_path_map:
+            strain_frac = (
+                pd.read_table(
+                    species_path_map[species],
+                    dtype=dict(strain=str, sample=str, community=float),
+                    index_col=["sample", "strain"],
+                )
+                .squeeze().unstack("strain", fill_value=0)
             )
-            .squeeze.unstack("strain", fill_value=0)
-            .reindex(sample_list, fill_value=0)
-        )
-        if motu not in motu_cv.columns:
-            motu_cv[motu] = 0
-        sotu_cv[motu] = (
-            frac.assign(other=1 - frac.sum(1))
-            .rename(columns=lambda strain_i: motu + "-" + str(strain_i))
-            .rename_axis(columns="sotu")
-            .multiply(motu_cv[motu], axis=0)
-            .stack()
-        )
-    # Add entries for all species that didn't have strain splits.
-    motu_cv_missing = motu_cv.rename(columns=lambda s: s + "-other").drop(
-        columns=[k + "-other" for k in sotu_cv.keys()]
-    )
-    sotu_cv = (
-        pd.concat(sotu_cv.values())
-        .unstack("sotu", fill_value=0)
-        .join(motu_cv_missing)
-        .rename_axis(columns="strain")
-    )
-    # FIXME: Some of the '-other' strains are < 0 because of rounding error.
-    sotu_cv[(sotu_cv < 0)] = 0
+            assert np.allclose(strain_frac.sum(1), 1.0, atol=1e-4)
+            # Drop existing dummy strain fraction (we'll add it back later).
+            strain_frac = (
+                strain_frac
+                .reindex(sample_list, fill_value=0)
+                .drop(columns=[DUMMY_STRAIN_LABEL], errors='ignore')
+            )
+            # Drop any strain fractions less than MINIMUM_FRAC.
+            strain_frac = strain_frac.where(strain_frac > MINIMUM_FRAC, 0)
+            # Add back the dummy strain fraction as everything needed to add
+            # up to 1.0.
+            strain_frac[DUMMY_STRAIN_LABEL] = 1 - strain_frac.sum("columns")
+            assert np.allclose(strain_frac.sum(1), 1.0, atol=1e-4)
+        else:
+            # When no strains were provided, mark everything as the dummy strain.
+            strain_frac = pd.DataFrame({DUMMY_STRAIN_LABEL: 1}, index=sample_list)
+        # Rename strains by prepending the species_id.
+        strain_frac = strain_frac.rename(columns=lambda s: species + '-' + s)
+        # Multiply by the species_depth to get the strain depth.
+        strain_depth[species] = (strain_frac.T * species_depth[species]).T
 
-    assert sotu_cv.index.is_unique
-    assert sotu_cv.columns.is_unique
-    assert (sotu_cv >= 0).all().all()
-
-    sotu_cv.stack().rename("depth")[lambda x: x > 0].to_csv(
-        outpath, sep="\t", header=True
-    )
+    strain_depth = pd.concat(strain_depth.values(), axis=1)
+    assert np.allclose(strain_depth.sum("columns"), species_depth.sum("columns"), atol=1e-4)
+    # Write stacked table (dropping 0s to keep the file-size small).
+    strain_depth.where(strain_depth > 0, np.nan).stack().dropna().rename("depth").to_csv(outpath, sep='\t')
