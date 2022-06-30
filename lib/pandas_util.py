faaaed2709f90f3bba13bf5001766f64d8fc530f
Byron Smith <me@byronjsmith.com>
2022-06-30 14:54:59 -0700

    Function for collecting describing of values in pandas series.

--- lib/pandas_util.py
+++ lib/pandas_util.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def idxwhere(condition, x=None):
     if x is None:
         x = condition
@@ -16,3 +18,10 @@ def align_indexes(*args):
 
     assert idx
     return [a.reindex(idx) for a in args]
+
+
+def repeated(df, subset=None):
+    if subset is None:
+        subset = df.columns
+    out = pd.Series((df[subset].values[1:] == df[subset].values[:-1]).all(1), index=df.index[1:])
+    return out.reindex(df.index, fill_value=False)
