6fd925f47a58ae6c5ad406ee655c8576689354c6
Byron Smith <me@byronjsmith.com>
2022-07-25 09:00:45 -0700

    Add library functions for fixed-width matplotlib ax grids.

--- lib/plot.py
+++ lib/plot.py
@@ -41,6 +41,25 @@ def construct_ordered_palette_from_list(x, colors=[], other="grey"):
         colormap[i] = c
     return colormap
 
+
+def subplots_grid(ncols, naxes, ax_width=3, ax_height=2.5, **kwargs):
+    nrows = int(np.ceil(naxes / ncols))
+    fig, axs = plt.subplots(
+        nrows, ncols, figsize=(ax_width * ncols, ax_height * nrows), **kwargs
+    )
+    axs = np.reshape(axs, (nrows, ncols))
+    return fig, axs
+
+
+def subplots_grid_fixed_rows(nrows, naxes, ax_width=3, ax_height=2.5, **kwargs):
+    ncols = int(np.ceil(naxes / nrows))
+    fig, axs = plt.subplots(
+        nrows, ncols, figsize=(ax_width * ncols, ax_height * nrows), **kwargs
+    )
+    axs = np.reshape(axs, (nrows, ncols))
+    return fig, axs
+
+
 def demo_palette(palette):
     df = pd.DataFrame(palete)
     print(df)
