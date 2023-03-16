20ce0dc3fb62b454cf9d1c380eb25609dd9ae0c4
Byron Smith <me@byronjsmith.com>
2023-03-16 09:56:18 -0700

    Update partitioning script.

--- scripts/partition_strain_samples.py
+++ scripts/partition_strain_samples.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import sys
+from lib.pandas_util import idxwhere, align_indexes
 
 
 if __name__ == "__main__":
@@ -13,16 +14,30 @@ if __name__ == "__main__":
     output_nospecies_path = sys.argv[6]
     output_strain_path = sys.argv[7]
 
-    species_depth = pd.read_table(
-        species_depth_path, index_col=["sample", "species_id"]
-    )
+    assert (
+        frac_thresh >= 0.5
+    ), "frac_thresh < 0.5 might prevent complete partitioning of samples."
 
-    assert len(species_depth.species_id.unique()) == 1
-    species_depth = species_depth.squeeze().unstack("species_id").squeeze()
+    species_depth = pd.read_table(
+        species_depth_path,
+        names=["sample", "depth"],
+        index_col="sample",
+    ).squeeze()
     strain_frac = (
         pd.read_table(strain_frac_path, index_col=["sample", "strain"])
         .squeeze()
         .unstack("strain")
     )
+    with open(output_nospecies_path, "w") as f:
+        for sample in idxwhere(species_depth < absent_thresh):
+            print(sample, file=f)
 
-    # species_depth <
+    species_depth, strain_frac = align_indexes(
+        species_depth, strain_frac, axis="index", how="inner"
+    )
+    strain_samples = (strain_frac > frac_thresh).apply(
+        lambda x: x & (species_depth > present_thresh)
+    )
+
+    strain_sample_output = strain_samples.T.stack()[lambda x: x].index
+    strain_sample_output.to_frame().to_csv(output_strain_path, sep="\t", index=False)
