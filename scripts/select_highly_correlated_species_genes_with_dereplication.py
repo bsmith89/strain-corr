f67f8f23b9f7c3f8663a20112030760fb5773e09
Byron Smith <me@byronjsmith.com>
2023-04-11 12:03:54 -0700

    Add updated `select_highly_correlated_species_genes_with_dereplication` script.

--- scripts/select_highly_correlated_species_genes_with_dereplication.py
+++ scripts/select_highly_correlated_species_genes_with_dereplication.py
@@ -3,11 +3,9 @@
 # USAGE: {input.script} \
 #     {input.species_depth} \
 #     {wildcards.species} \
-#     {params.absent_thresh} \
-#     {params.present_thresh} \
-#     {input.strain_frac} \
-#     {params.frac_thresh} \
+#     {params.diss_thresh} \
 #     {input.gene_depth} \
+#     {params.trnsf_root} \
 #     {params.n_marker_genes} \
 #     {output.species_gene} \
 #     {output.species_corr} \
@@ -15,108 +13,81 @@
 import sys
 import pandas as pd
 import xarray as xr
-from scipy.spatial.distance import cdist
 from lib.pandas_util import idxwhere
 from lib.util import info
+from scipy.spatial.distance import cdist, pdist, squareform
+from sklearn.cluster import AgglomerativeClustering
 
 if __name__ == "__main__":
     species_depth_inpath = sys.argv[1]  # {input.species_depth}
     species_id = sys.argv[2]  # {wildcards.species}
-    absent_thresh = float(sys.argv[3])  # {params.absent_thresh}
-    present_thresh = float(sys.argv[4])  # {params.present_thresh}
-    strain_frac_inpath = sys.argv[5]  # {input.strain_frac}
-    frac_thresh = float(sys.argv[6])  # {params.frac_thresh}
-    gene_depth_inpath = sys.argv[7]  # {input.gene_depth}
-    root = float(sys.argv[8])  # {params.trnsf_root}
-    n_marker_genes = int(sys.argv[9])  # {params.n_marker_genes}
-    species_gene_outpath = sys.argv[10]  # {output.species_gene}
-    species_corr_outpath = sys.argv[11]  # {output.species_corr}
+    diss_thresh = float(sys.argv[3])  # {params.diss_thresh}
+    gene_depth_inpath = sys.argv[4]  # {input.gene_depth}
+    root = float(sys.argv[5])  # {params.trnsf_root}
+    n_marker_genes = int(sys.argv[6])  # {params.n_marker_genes}
+    species_gene_outpath = sys.argv[7]  # {output.species_gene}
+    species_corr_outpath = sys.argv[8]  # {output.species_corr}
 
     info("Loading input data.")
     info("Loading species depth.")
     species_depth = (
         pd.read_table(
             species_depth_inpath,
-            dtype={"species_id": str},
+            dtype={"sample": str, "species_id": str, "depth": float},
             index_col=["sample", "species_id"],
         )
         .squeeze()
-        .unstack(fill_value=0)[species_id]
         .to_xarray()
+        .sel(species_id=species_id)
+        .fillna(0)
     )
-
-    info("Loading strain composition.")
-    strain_frac = pd.read_table(
-        strain_frac_inpath, index_col=["sample", "strain"]
-    ).community.to_xarray()
-
     info("Loading gene depth.")
-    gene_depth = xr.load_dataarray(gene_depth_inpath).fillna(0)
-
-    info("Identifying strain-pure samples.")
-    sample_to_strain = (
-        (strain_frac >= frac_thresh)
-        .to_series()[lambda x: x]
-        .index.to_frame()[[]]
-        .reset_index("strain")
-        .squeeze()
-        .to_xarray()
-    )
+    gene_depth = xr.load_dataarray(gene_depth_inpath).fillna(
+        0
+    )  # FIXME: Shouldn't be necessary.
 
     info("Aligning indexes.")
     sample_list = list(set(gene_depth.sample.values) & set(species_depth.sample.values))
     species_depth = species_depth.sel(sample=sample_list)
     gene_depth = gene_depth.sel(sample=sample_list)
-    sample_to_strain = sample_to_strain.sel(
-        sample=list(set(sample_to_strain.sample.values) & set(sample_list))
-    )
-    nospecies_samples = idxwhere(species_depth.to_series() < absent_thresh)
 
-    info("De-replicating strain-pure samples.")
-    species_depth_agg = (
-        species_depth.sel(sample=sample_to_strain.sample)
-        .groupby(sample_to_strain)
-        .sum()
+    n_samples = len(gene_depth.sample)
+    info(f"Calculating cosine dissimilarity of gene depths among {n_samples} samples.")
+    cdmat = pdist(gene_depth.to_pandas(), metric="cosine")
+    info(
+        f"De-replicating samples using agglomorative clustering with a cosine dissimilarity threshold of {diss_thresh}."
     )
-    gene_depth_agg = (
-        gene_depth.sel(sample=sample_to_strain.sample).groupby(sample_to_strain).sum()
-    )
-
-    if species_depth_agg.sizes["strain"] == 1:
-        info(
-            "WARNING: Only one strain group remaining after de-replication; this will skew the results."
+    clust = (
+        pd.Series(
+            AgglomerativeClustering(
+                n_clusters=None,
+                affinity="precomputed",
+                linkage="average",
+                distance_threshold=diss_thresh,
+            ).fit_predict(squareform(cdmat)),
+            index=gene_depth["sample"],
+            name="clust",
         )
-
-    info("Concatenating strain samples with no-species samples.")
-    species_depth = xr.concat(
-        [
-            species_depth_agg.rename({"strain": "sample"}),
-            species_depth.sel(sample=nospecies_samples),
-        ],
-        dim="sample",
+        .rename_axis(index="sample")
+        .to_xarray()
     )
-    gene_depth = xr.concat(
-        [
-            gene_depth_agg.rename({"strain": "sample"}),
-            gene_depth.sel(sample=nospecies_samples),
-        ],
-        dim="sample",
-    )
+    species_depth = species_depth.groupby(clust).sum()
+    gene_depth = gene_depth.groupby(clust).sum()
+    n_clusts = len(gene_depth.clust)
+    info(f"Found {n_clusts} de-replicated clusters.")
 
-    info(f"Transforming depth data. (root-{root}).")
-    trnsf = lambda x: x ** (1 / root)
-    species_depth = species_depth.pipe(trnsf)
-    gene_depth = gene_depth.pipe(trnsf)
+    info("Transforming input data.")
+    species_depth = species_depth.pipe(lambda x: x ** (1 / root))
+    gene_depth = gene_depth.pipe(lambda x: x ** (1 / root))
 
     n_genes = len(gene_depth.gene_id)
-    n_samples = len(gene_depth.sample)
-    info(f"Calculating correlation among {n_genes} genes across {n_samples} samples.")
+    info(f"Calculating correlation of {n_genes} genes.")
     corr = pd.Series(
         1
         - cdist(
             species_depth.expand_dims(dict(_=1)),
-            gene_depth.transpose("gene_id", "sample"),
-            metric="cosine",
+            gene_depth.transpose("gene_id", "clust"),
+            metric="cosine",  # lambda x, y: sp.stats.spearmanr(x, y)[0],
         )[0],
         index=gene_depth.gene_id,
     )
@@ -125,7 +96,7 @@ if __name__ == "__main__":
     corr_thresh = corr.sort_values(ascending=False).head(n_marker_genes).min()
     species_gene_hit = idxwhere(corr >= corr_thresh)
     nhits = len(species_gene_hit)
-    info(f"Found {nhits} highly correlated genes (cosine similarity > {corr_thresh}).")
+    info(f"Found {nhits} highly correlated genes (correlation > {corr_thresh}).")
 
     info("Writing species gene list.")
     with open(species_gene_outpath, "w") as f:
