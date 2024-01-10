b19f349564552ac91ee872ca33900578ea58e845
Byron Smith <me@byronjsmith.com>
2024-01-10 12:34:34 -0800

    New implementation of 'voting' procedure for transfering annotations to c75s

--- scripts/aggregate_gene99_annotations_to_higher_centroid.py
+++ scripts/aggregate_gene99_annotations_to_higher_centroid.py
@@ -5,12 +5,6 @@ import sys
 import pandas as pd
 
 
-def vote_on_annotations(df, annot_col):
-    num_genes = len(df.gene_id.unique())
-    counts = df[annot_col].value_counts()
-    return counts / num_genes
-
-
 if __name__ == "__main__":
     annot_inpath = sys.argv[1]
     gene_info_inpath = sys.argv[2]
@@ -20,16 +14,26 @@ if __name__ == "__main__":
     annot_table = pd.read_table(annot_inpath)
     assert annot_table.columns[0] == "centroid_99"
     annot_name = annot_table.columns[1]
-    gene_agg = pd.read_table(gene_info_inpath)[["gene_id", "centroid_99", agg_column]]
-    all_votes = pd.merge(annot_table, gene_agg, on="centroid_99")
-    agg_annot_votes = all_votes.groupby(agg_column).apply(
-        vote_on_annotations, annot_name
-    )
+
+    all_gene_agg = pd.read_table(gene_info_inpath)[
+        ["gene_id", "centroid_99", agg_column]
+    ]
+    assert all_gene_agg.gene_id.is_unique
+
+    # Count the total number of genes per aggregate level (e.g. centroid_75).
+    out_of = all_gene_agg[agg_column].value_counts().rename_axis(agg_column)
+
+    # Pare-down the table before merging.
+    gene_agg = all_gene_agg[lambda x: x.centroid_99.isin(annot_table.centroid_99)]
+    votes_for = pd.merge(annot_table, gene_agg, on="centroid_99")[
+        [agg_column, annot_name]
+    ].value_counts()
 
     result = (
-        agg_annot_votes[lambda x: x >= 0.5]
+        (votes_for / out_of)[lambda x: x >= 0.5]
         .rename_axis(index=(agg_column, annot_name))
         .to_frame("fraction")
-        .reset_index()[[agg_column, annot_name]]
     )
-    result.to_csv(outpath, sep="\t", index=False)
+    result.reset_index()[[agg_column, annot_name]].to_csv(
+        outpath, sep="\t", index=False
+    )
