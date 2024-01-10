a10aef2440b1a2c3b4d28328f151d888b27f3a44
Byron Smith <me@byronjsmith.com>
2024-01-10 13:32:22 -0800

    Add missing GO-term parser.

---
+++ scripts/parse_emapper_output_to_gene_x_go.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import sys
+import numpy as np
+
+EGGNOG_COLUMNS = [
+    "query",
+    "seed_ortholog",
+    "evalue",
+    "score",
+    "eggNOG_OGs",
+    "max_annot_lvl",
+    "COG_category",
+    "Description",
+    "Preferred_name",
+    "GOs",
+    "EC",
+    "KEGG_ko",
+    "KEGG_Pathway",
+    "KEGG_Module",
+    "KEGG_Reaction",
+    "KEGG_rclass",
+    "BRITE",
+    "KEGG_TC",
+    "CAZy",
+    "BiGG_Reaction",
+    "PFAMs",
+    "contig_id",
+    "start",
+    "end",
+    "strand",
+    "gene_type",
+    "qcontig_length",
+]
+
+
+if __name__ == "__main__":
+    eggnog = (
+        pd.read_table(sys.argv[1], comment="#", names=EGGNOG_COLUMNS, index_col="query")
+        .rename_axis(index="centroid_99")
+        .replace({"-": np.nan})
+    )
+    gene_x_unit = (
+        eggnog.GOs.dropna()
+        .str.split(",")
+        .explode()
+        .rename("go")
+    )
+    gene_x_unit.to_csv(sys.argv[2], sep="\t")
