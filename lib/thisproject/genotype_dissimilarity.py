06710ee80ff5c90c87ac9a73a8de1a89422fee2b
Byron Smith <me@byronjsmith.com>
2024-10-22 02:57:58 -0700

    Update Spikein benchmark to a working first version.

--- lib/thisproject/genotype_dissimilarity.py
+++ lib/thisproject/genotype_dissimilarity.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import pdist, cdist
 
 def native_masked_hamming_distance_pdist(X, pseudo=0):
     isnan_X = np.isnan(X)
@@ -8,6 +8,22 @@ def native_masked_hamming_distance_pdist(X, pseudo=0):
     cityblock = displaced_cityblock - (mismatched_nan_count / 2)
     anynan_count = (
         np.nan_to_num(pdist(isnan_X, metric="kulczynski1"), nan=0)
+        * mismatched_nan_count
+        + mismatched_nan_count
+    )
+    unmasked_positions = X.shape[1] - anynan_count
+    masked_hamming_distance = (cityblock + pseudo) / (unmasked_positions + pseudo)
+    return masked_hamming_distance
+
+
+def native_masked_hamming_distance_cdist(X, Y, pseudo=0):
+    assert X.shape[1] == Y.shape[1]
+    isnan_X, isnan_Y = np.isnan(X), np.isnan(Y)
+    displaced_cityblock = cdist(np.nan_to_num(X, nan=0.5), np.nan_to_num(Y, nan=0.5), metric="cityblock")
+    mismatched_nan_count = cdist(isnan_X, isnan_Y, metric="cityblock")
+    cityblock = displaced_cityblock - (mismatched_nan_count / 2)
+    anynan_count = (
+        np.nan_to_num(cdist(isnan_X, isnan_Y, metric="kulczynski1"), nan=0)
         * mismatched_nan_count
         + mismatched_nan_count
     )
